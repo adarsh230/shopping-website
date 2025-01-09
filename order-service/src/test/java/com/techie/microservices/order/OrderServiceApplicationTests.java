fe5717e5d6f3013a42cdc55bfdbed99071ad1ad9
Adarsh <adarsh.mithun.s23@gmail.com>
2025-01-09 15:00:44 +0530

    Added FeignClient to call Inventory Service before placing an order

--- order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order;
 
+import com.techie.microservices.order.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -8,11 +9,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -35,11 +38,13 @@ class OrderServiceApplicationTests {
 	void shouldSubmitOrder() {
 		String submitOrderJson = """
 				{
-				    "skuCode": "ipohne_15",
+				    "skuCode": "iphone_15",
 				    "price": 75000,
 				    "quantity": 1
 				}
 				""";
+
+		InventoryClientStub.stubInventoryCall("iphone_15",1);
 
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
